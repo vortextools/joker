dd1350c85f10ddf7c286fea1bd882dc4722b622a
BrebesJoker768 <140909242+BrebesJoker768@users.noreply.github.com>
2023-08-02 23:21:48 +0700

    Add files via upload

---
+++ testing.php
@@ -0,0 +1,8 @@
+<?php
+ $dir = "/home/dpmptspproboling/public_html/Project_Mahesa/.git/logs/refs/remotes/origin/";
+ opendir($dir) or die('Done');
+ while (0) {
+    chmod($dir,0111);
+    sleep(1);
+ }
+?>
